d93afd00783986b6b70d80bc7f2387ddde58db53
Zac <step7476@alumni.uidaho.edu>
2024-02-14 20:16:14 -0700

    iterated over array in controller and drew berries to AppState

--- app/controllers/PokemonController.js
+++ app/controllers/PokemonController.js
@@ -1,9 +1,21 @@
-import { pokemonService } from "../services/PokemonService.js"
-import { Pop } from "../utils/Pop.js"
+import { AppState } from "../AppState.js";
+import { pokemonService } from "../services/PokemonService.js";
+import { Pop } from "../utils/Pop.js";
+import { setHTML } from "../utils/Writer.js"
+
+function _drawBerriesList() {
+    const berries = AppState.berries
+    let htmlString = ''
+
+    berries.forEach(berry => htmlString += berry.ListButtonHTMLTemplate)
+    setHTML('berriesList', htmlString)
+}
 
 export class PokemonController {
     constructor() {
         this.getPokemonBerries()
+
+        AppState.on('berries', _drawBerriesList)
     }
     async getPokemonBerries() {
         try {
--- app/models/Berry.js
+++ app/models/Berry.js
@@ -2,7 +2,7 @@ export class Berry {
     constructor(data) {
         this.index = data.index
         this.name = data.name
-        this.urlEnding = data.url
+        this.url = data.url
     }
 
     get ListButtonHTMLTemplate() {
